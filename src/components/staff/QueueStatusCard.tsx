bc5b146ec45f68a2917f3ccc18545a4907a7e09e
Kashmala-Asad <kashmalaasad84@gmail.com>
2025-12-30 01:39:10 +0500

    Update QueueStatusCard.tsx

--- src/components/staff/QueueStatusCard.tsx
+++ src/components/staff/QueueStatusCard.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Badge } from '../ui/Badge';
 import { Queue } from '@/types';
 
-
+interface QueueStatusCardProps {
+  queueItem: Queue;
+}
 
 export const QueueStatusCard: React.FC<QueueStatusCardProps> = ({ queueItem }) => {
   const getStatusVariant = (status: string) => {
