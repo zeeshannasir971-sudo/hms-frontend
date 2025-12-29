abf4f86c64fe16be94abdebbe33d03e83006e81a
Kashmala-Asad <kashmalaasad84@gmail.com>
2025-12-30 01:44:12 +0500

    Update QueuePatientCard.tsx

--- src/components/doctor/QueuePatientCard.tsx
+++ src/components/doctor/QueuePatientCard.tsx
@@ -6,6 +6,7 @@ import { Queue } from '@/types';
 interface QueuePatientCardProps {
   queueItem: Queue;
   onStartConsultation: (queueId: string) => void;
+  onCompleteConsultation: (queueId: string) => void;
 
 }
 
